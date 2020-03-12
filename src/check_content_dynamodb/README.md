d8bf5484e9e5ccec8dacd9093e7b138d8cd8cb3d
Hiro Watari <watari.codes@gmail.com>
2020-03-12 15:30:33 -0700

    Added lambda function for checking content in DynamoDB

---
+++ src/check_content_dynamodb/README.md
@@ -0,0 +1,4 @@
+This lambda function requires the folloiwng environment variables:
+
+`DYNAMODB_TABLE`:`xxxx`
+`DYNAMODB_KEY`:`xxxx`
---
+++ src/check_content_dynamodb/lambda_function.py
@@ -0,0 +1,20 @@
+import os
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+def lambda_handler(event, context):
+    
+    content = event['content']
+    
+    dynamodb = boto3.resource('dynamodb', region_name = os.environ['AWS_REGION'])
+    
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    
+    response = table.query(
+        KeyConditionExpression=Key(os.environ['DYNAMODB_KEY']).eq(content)
+    )
+    
+    return {
+        'exists': bool(response['Count'])
+    }
+
