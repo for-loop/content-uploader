5386b1fd81ea4dac92a4763a07ace0fb1b4a2338
Hiro Watari <31600246+for-loop@users.noreply.github.com>
2020-03-13 17:23:01 -0700

    Sped up query by up to 6x faster

--- src/check_content_dynamodb/lambda_function.py
+++ src/check_content_dynamodb/lambda_function.py
@@ -2,13 +2,12 @@ import os
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
+dynamodb = boto3.resource('dynamodb', region_name = os.environ['AWS_REGION'])
+table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
 def lambda_handler(event, context):
     
     content = event['content']
-    
-    dynamodb = boto3.resource('dynamodb', region_name = os.environ['AWS_REGION'])
-    
-    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
     
     response = table.query(
         KeyConditionExpression=Key(os.environ['DYNAMODB_KEY']).eq(content)
